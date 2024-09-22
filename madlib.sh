4e75ec0a6b2c224913a14bcf924c900113220a4f
Madeleine Cummings <uwg9at@virginia.edu>
2024-09-22 00:30:02 -0400

    lab3 Mad-lib

---
+++ madlib.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. NAME AN UNUSUAL SPORT: " NOUN1
+read -p "2. NAME AN ANIMAL: " NOUN2
+read -p "3. NAME A PLACE: " NOUN3
+read -p "4. NAME A VERB: " NOUN4
+read -p "5. NAME A MODE OF TRANSPORTATION: " NOUN5
+read -p "6. NAME AN OBJECT: " NOUN6
+read -p "7. NAME AN ACTION PAST TENSE: " NOUN7
+read -p "8. NAME A DESCRIPTION WORD: " NOUN8
+
+echo "Once upon a time, my friend Sona and I were playing $NOUN1.  And all of a sudden we saw a giant $NOUN2.  It started to chase us all the way down $NOUN3.  It wasn't until we started $NOUN4, that it stopped chasing us and was mezmerized by our talents.  We quickly used this moment escape via $NOUN5.  That was not the last of our troubles though... the $NOUN2 had caught up with us!  We suddenly saw a $NOUN6 and used it to fight the $NOUN2 off.  It was a hard task but we finally had won.  We went out to celebrate and $NOUN7 the night away.  It was an $NOUN8 night."
+  
